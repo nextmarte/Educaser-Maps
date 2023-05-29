50570c49b2fd3c090370b985512a2bce6bb14493
Marcus Ramalho <marcusantonio@id.uff.br>
2023-05-29 01:32:04 -0300

    mapa

--- scripts/capitaniascintra.R
+++ scripts/capitaniascintra.R
@@ -1,136 +1,10 @@
 library(leaflet)
 library(sf)
+library(tidyverse)
 
-coordenadas <- data.frame(
-  n = c(
-    "1 A",
-    "2A",
-    "3",
-    "4",
-    "1 B",
-    "2B",
-    "12A",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "11A",
-    "12B",
-    "11B",
-    "12C",
-    "",
-    ""
-  ),
-  CAPITANIA = c(
-    "Maranhao 1",
-    "Maranhao 2",
-    "Piaui",
-    "Ceara",
-    "Rio Grande do Norte 1",
-    "Rio Grande do Norte 2",
-    "Itamaraca",
-    "Pernanbuco",
-    "Bahia",
-    "Ilheus",
-    "Porto Seguro",
-    "Espirito Santo",
-    "Sao Tome",
-    "Sao Vicente 1",
-    "Santo Amaro",
-    "Sao Vicente 2",
-    "Santana",
-    "Limite sul 28 e 1/3",
-    "Linha de Tordesilhas"
-  ),
-  DONATARIO = c(
-    "Aires da Cunha",
-    "Joao de Barros",
-    "Fernando Alvares de Andrade",
-    "Antonio Cardoso de Barros",
-    "Aires da Cunha",
-    "Joao de Barros",
-    "Pero Lopes de Sousa",
-    "Duarte Coelho [Pereira]",
-    "Francisco Pereira Coutinho",
-    "Jorge de Figueiredo Correia",
-    "Pedro do Campo Tourinho",
-    "Vasco Fernandes Coutinho",
-    "Pero de Gois [da Silveira]",
-    "Martim Afonso de Sousa",
-    "Pero Lopes de Sousa",
-    "Martim Afonso de Sousa",
-    "Pero Lopes de Sousa",
-    "Limite sul 28 e 1/3",
-    "Linha de Tordesilhas"
-  ),
-  LIMITE = c(
-    "Rio Turiacu",
-    "Ponto intermediario",
-    "Ilha de Santana (oeste)",
-    "Camocim",
-    "Mucuripe",
-    "Ponto intermediario",
-    "Baia da Traicao",
-    "Sul da I. de Itamaraca",
-    "Rio de Sao Francisco",
-    "Sul da Baia de TS",
-    "Rio Pardo",
-    "Rio Mucuri",
-    "Rio Itapemirim",
-    "Rio Macae",
-    "Rio Juquiriquere",
-    "Barra da Bertioga",
-    "Barra sul de Paranagua",
-    "48.70",
-    "48.70"
-  ),
-  Lon = c(
-    45.25,
-    44.36,
-    43.75,
-    40.85,
-    38.46,
-    36.66,
-    34.93,
-    34.85,
-    36.40,
-    38.81,
-    38.95,
-    39.56,
-    40.81,
-    41.78,
-    45.43,
-    46.13,
-    48.36,
-    48.70,
-    48.70
-  ),
-  Lat = c(
-    1.64,
-    2.33,
-    2.36,
-    2.87,
-    3.72,
-    5.08,
-    6.68,
-    7.81,
-    10.50,
-    13.14,
-    15.65,
-    18.09,
-    21.00,
-    22.38,
-    23.71,
-    23.86,
-    25.55,
-    28.33,
-    0
-  )
-)
+coordenadas <- read.csv("https://raw.githubusercontent.com/nextmarte/Educaser-Maps/main/dados/tabela_cintra.csv")
 
- rio::export(coordenadas,"tabela-cintra","csv")
+ #rio::export(coordenadas,"tabela-cintra","csv")
 
 
 #temos que multiplicar por -1
@@ -145,6 +19,8 @@ mapa <- leaflet(coordenadas) %>%
   addTiles() %>%
   setView(lng = -45, lat = -15, zoom = 4)  # Configurar a visualização inicial do mapa
 
+mapa
+
 # Adicionar marcadores para cada Capitania Hereditária
 mapa <- mapa %>%
   addMarkers(
@@ -158,22 +34,38 @@ mapa <- mapa %>%
 
 mapa
 
-# Adicionar linhas latitudinais
-# for (lat in latitudes) {
-#   for(lng in longitude){
-#   mapa <- mapa %>%
-#     addPolylines(
-#       data = NULL,
-#       lng = c(-48.7,lng),  # Longitude para traçar a linha latitudinal
-#       lat = c(lat,lat),  # Latitudes para traçar a linha latitudinal
-#       color = "red",
-#       weight = 2
-#     )
-# }
-# }
-# mapa
-# Exibir o mapa
+# working linhas horizontais ------------------------------------------------------------------
 
+
+for (i in 17:7) {
+  mapa <- mapa %>%
+    addPolylines(
+      lng = c(coordenadas$Lon[i], min(longitude)),
+      lat = c(coordenadas$Lat[i], latitudes[i]),
+      color = "red",
+      weight = 2,
+      label = paste0("latitude: ",coordenadas$Lat[i])
+    )
+}
+
+mapa
+
+
+# poligonos-teste ---------------------------------------------------------
+
+for (i in 1:(length(latitudes) - 1)) {
+  mapa <- mapa %>%
+    addPolygons(
+      lng = c(longitude[i], longitude[i+1], longitude[i+1], longitude[i]),
+      lat = c(latitudes[i], latitudes[i+1], latitudes[i+1], latitudes[i]),
+      color = "blue",
+      fill = TRUE,
+      fillOpacity = 0.5
+    )
+}
+
+mapa
+# desenho -----------------------------------------------------------------
 
 
 mapa <- mapa %>%
@@ -188,7 +80,7 @@ mapa <- mapa %>%
     weight = 1
   )
 
-
+mapa
 
 
 # Adicionar polígonos para delimitar cada Capitania Hereditária
